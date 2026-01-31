a8f88d188d0a0b67a53406165592d9633f50d084
jiker666 <2224328047@qq.com>
2026-02-01 04:20:36 +0800

    Simple C print

--- test.sh
+++ test.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 echo "Compiling..."
-if command -v gcc &> /dev/null; then
-    gcc exploit.c -o exploit
-else
-    cc exploit.c -o exploit
-fi
-
-python3 -c "import os; t='/rea'+'dflag'; os.environ['MY_TARGET']=t; os.system('./exploit')"
+gcc exploit.c -o exploit
+./exploit
